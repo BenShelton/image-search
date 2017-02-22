5cd5dcaf12747ac90409609fbf7cc4e718758953
Ben Jones <bensheltonjones@yahoo.co.uk>
2017-02-22 22:15:14 +0000

    Stable version

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // dependencies
 const express = require('express');
 const mongodb = require('mongodb');
+const requestify = require('requestify');
 
 // environment variables
 const port = process.env.PORT;
 const mongoUrl = process.env.MONGODB_URI;
+const pixKey = process.env.PIXABAY_API;
 
 // server & db setup
 const app = express();
@@ -26,7 +28,59 @@ mongodb.MongoClient.connect(mongoUrl, (err, db) => {
 // setup static files
 app.use(express.static(__dirname + '/public'));
 
-// routing **
+// routing
+
+// recent searches route
+app.get('/latest-searches', (req, res) => {
+  col.find({}, {'_id': 0}).sort({_id:-1}).limit(10).toArray((err, docs) => {
+    if (err) {
+      console.log(err);
+      res.json({error: "Could not access Database"});
+    } else {
+      res.json(docs);
+    }
+  });
+});
+
+// image search route
+app.get('/:search', (req, res) => {
+  // query params for search
+  var options = {
+    params: {
+      key: pixKey,
+      q: req.params.search,
+      page: req.query.page || 1,
+      per_page: 10
+    }
+  };
+  // send request
+  requestify.get('https://pixabay.com/api/', options)
+    .then(apiRes => {
+      // record search in db
+      col.insert({
+        search: req.params.search,
+        time: new Date().toString()
+      });
+      // extract useful info
+      var output = [];
+      apiRes.getBody().hits.forEach(img => {
+        output.push({
+          type: img.type,
+          tags: img.tags,
+          views: img.views,
+          likes: img.likes,
+          pageURL: img.pageURL
+        });
+      });
+      // send JSON to client
+      res.json(output);
+    })
+    .fail(apiRes => {
+      console.log(apiRes.getCode());
+      res.json({error: 'Search Failed'});
+    }
+  );
+});
 
 // 404 handling
 app.use((req, res) => {
